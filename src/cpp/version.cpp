8649c97d505d204b5bfb5fb5bd6101e186bfd59a
Ricardo Gonzalez <ricardogonzalez@eprosima.com>
2013-06-20 10:16:44 +0000

    - Added version to CDR library.

---
+++ src/cpp/version.cpp
@@ -0,0 +1,4 @@
+#define VERSION_STR "0.1.0"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_RELEA 0
--- utils/scripts/build_cdr.bat
+++ utils/scripts/build_cdr.bat
@@ -13,7 +13,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="ReleaseDLL" /p:
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="ReleaseDLL" /p:Platform="Win32"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: Debug DLL Configuration
 :: Clean the visual solution
@@ -21,7 +21,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="DebugDLL" /p:Pl
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="DebugDLL" /p:Platform="Win32"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: Release Configuration
 :: Clean the visual solution
@@ -29,7 +29,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="Release" /p:Pla
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="Release" /p:Platform="Win32"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: Debug Configuration
 :: Clean the visual solution
@@ -37,7 +37,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="Debug" /p:Platf
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="Debug" /p:Platform="Win32"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: x64 Platform
 
@@ -47,7 +47,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="ReleaseDLL" /p:
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="ReleaseDLL" /p:Platform="x64"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: Debug DLL Configuration
 :: Clean the visual solution
@@ -55,7 +55,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="DebugDLL" /p:Pl
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="DebugDLL" /p:Platform="x64"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: Release Configuration
 :: Clean the visual solution
@@ -63,7 +63,7 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="Release" /p:Pla
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="Release" /p:Platform="x64"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
 :: Debug Configuration
 :: Clean the visual solution
@@ -71,9 +71,12 @@ msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Clean /p:Configuration="Debug" /p:Platf
 :: Build the visual solution
 msbuild "..\..\win32\cpp\CDR\CDR.sln" /t:Build /p:Configuration="Debug" /p:Platform="x64"
 set errorstatus=%ERRORLEVEL%
-if not %errorstatus%==0 goto :EOF
+if not %errorstatus%==0 goto :exit
 
-echo "BUILD SUCCESSFULLY"
+goto :exit
 
+:: Function exit ::
+:exit
+if %errorstatus%==0 (echo "BUILD SUCCESSFULLY") else (echo "BUILD FAILED")
 goto :EOF
 

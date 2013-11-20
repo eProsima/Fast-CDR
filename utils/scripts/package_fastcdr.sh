b74576d4a2257925e66c1cf3a3ae37a012eb6405
Ricardo Gonzalez <ricardogonzalez@eprosima.com>
2013-11-20 09:09:33 +0000

    Added packaging script for Linux.

---
+++ utils/scripts/package_fastcdr.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+#: This script pack FastCDR product for any platform in Linux.
+#
+# This script needs the next programs to be run.
+# - subversion
+# Also this script needs the eProsima.documentation.changeVersion macro installed in the system.
+#
+# Fedora 19: This script has to be run in a Fedora 17 x64.
+#            The system has to have installed: libgcc.i686 and libstdc++.i686
+
+errorstatus=0
+
+function setPlatform
+{
+    platforms=`cat src/platforms`
+    if [[ $platforms != *$1* ]]; then
+        echo $1 >> src/platforms
+    fi
+}
+
+function package
+{
+    # Get current version of GCC.
+    . $EPROSIMADIR/scripts/common_pack_functions.sh getGccVersion
+
+    # Update and compile CDR library.
+    # Update CDR library.
+    svn update
+    errorstatus=$?
+    if [ $errorstatus != 0 ]; then return; fi
+    # Compile CDR library for i86.
+    rm -rf output
+    EPROSIMA_TARGET="i86Linux2.6gcc${gccversion}"
+    rm -r lib/$EPROSIMA_TARGET
+    make
+    errorstatus=$?
+    if [ $errorstatus != 0 ]; then return; fi
+    # Compile CDR library for x64.
+    rm -rf output
+    EPROSIMA_TARGET="x64Linux2.6gcc${gccversion}"
+    rm -r lib/$EPROSIMA_TARGET
+    make
+    errorstatus=$?
+    if [ $errorstatus != 0 ]; then return; fi
+
+    # Get the current version of FastBuffers
+    . $EPROSIMADIR/scripts/common_pack_functions.sh getVersionFromCPP fastcdrversion include/cdr/Cdr_version.h
+    errorstatus=$?
+    if [ $errorstatus != 0 ]; then return; fi
+
+    # Create installers
+    cd utils/installers/linux
+    ./setup_linux.sh $fastcdrversion
+    errorstatus=$?
+    cd ../../..
+    if [ $errorstatus != 0 ]; then return; fi
+
+    # Remove the doxygen tmp directory
+    rm -rf output
+}
+
+# Check that the environment.sh script was run.
+if [ "$EPROSIMADIR" == "" ]; then
+    echo "environment.sh must to be run."
+    exit -1
+fi
+
+# Go to root
+cd ../..
+
+package
+
+if [ $errorstatus == 0 ]; then
+    echo "PACKAGING SUCCESSFULLY"
+else
+    echo "PACKAGING FAILED"
+fi
+
+exit $errorstatus

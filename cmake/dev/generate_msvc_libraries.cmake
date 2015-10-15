f7ba2829ecdd7c8d02224060357ca85a9bcd6bf3
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2015-10-15 12:53:18 +0200

    Refs #1321. Fixed bugs

--- cmake/dev/generate_msvc_libraries.cmake
+++ cmake/dev/generate_msvc_libraries.cmake
@@ -22,9 +22,7 @@ macro(generate_msvc_libraries platform)
         )
 
     add_custom_target(${PROJECT_NAME}_${platform} ALL
-        COMMAND ${CMAKE_COMMAND} -G "${GENERATOR}" -DEPROSIMA_BUILD=ON -DLIB_INSTALL_DIR:PATH=lib/${platform} -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/eprosima_installer/${platform}/install ../../../..
-        COMMAND ${CMAKE_COMMAND} --build . --config Release
-        COMMAND ${CMAKE_COMMAND} --build . --config Debug
+        COMMAND ${CMAKE_COMMAND} -G "${GENERATOR}" -DEPROSIMA_BUILD=ON -DMINION=ON -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/eprosima_installer/${platform}/install ../../../..
         COMMAND ${CMAKE_COMMAND} --build . --target install --config Release
         COMMAND ${CMAKE_COMMAND} --build . --target install --config Debug
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/eprosima_installer/${platform}
@@ -35,7 +33,7 @@ endmacro()
 
 macro(install_msvc_libraries platform)
     install(DIRECTORY ${PROJECT_BINARY_DIR}/eprosima_installer/${platform}/install/lib/
-        DESTINATION lib/${platform}
+        DESTINATION ${LIB_INSTALL_DIR}/${platform}
         COMPONENT libraries_${platform}
         )
 

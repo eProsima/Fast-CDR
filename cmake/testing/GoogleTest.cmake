857b89a891bd1a8bf19894dd571f4605af42fb0d
Ricardo González <correoricky@gmail.com>
2023-07-05 11:53:52 +0200

    Refs #18687. Fix execution of test in directories with spaces
    Signed-off-by: Ricardo González <correoricky@gmail.com>

--- cmake/testing/GoogleTest.cmake
+++ cmake/testing/GoogleTest.cmake
@@ -4,9 +4,9 @@ function(gtest_discover_tests TARGET)
     if (WIN32)
         add_custom_command(
             TARGET ${TARGET} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E echo "set(_target_runtime_dlls $<TARGET_RUNTIME_DLLS:${TARGET}>)" > "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_runtime_dlls_$<CONFIG>.cmake"
-            COMMAND ${CMAKE_COMMAND} -DTARGET=${TARGET} -DCONFIG=$<CONFIG> -DFILE_TO_PROCESS="${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_runtime_dlls_$<CONFIG>.cmake" -P ${CMAKE_SOURCE_DIR}/cmake/testing/generate_google_test_win_wrapper.cmake
+            COMMAND ${CMAKE_COMMAND} -DTARGET=${TARGET} -DCONFIG=$<CONFIG> -DRUNTIME_LIST=$<TARGET_RUNTIME_DLLS:${TARGET}> -P ${CMAKE_SOURCE_DIR}/cmake/testing/generate_google_test_win_wrapper.cmake
             COMMAND_EXPAND_LISTS
+            VERBATIM
             )
 
         set(CMAKE_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_win_wrapper_$<CONFIG>.bat") 

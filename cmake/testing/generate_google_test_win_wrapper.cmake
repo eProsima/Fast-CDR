773da963d43356158a871166080b29541687295b
Ricardo González <correoricky@gmail.com>
2023-07-05 11:53:52 +0200

    Refs #18687. Fix execution of test in directories with spaces
    Signed-off-by: Ricardo González <correoricky@gmail.com>

--- cmake/testing/generate_google_test_win_wrapper.cmake
+++ cmake/testing/generate_google_test_win_wrapper.cmake
@@ -4,14 +4,13 @@ endif()
 if (NOT DEFINED CONFIG)
     message(FATAL_ERROR "This scrips needs CONFIG variable set")
 endif()
-if (NOT DEFINED FILE_TO_PROCESS)
-    message(FATAL_ERROR "This scrips needs FILE_TO_PROCESS variable set")
+if (NOT DEFINED RUNTIME_LIST)
+    message(FATAL_ERROR "This scrips needs RUNTIME_LIST variable set")
 endif()
 
-include(${FILE_TO_PROCESS})
-
 set(_path "")
-foreach(_runtime_dll ${_target_runtime_dlls})
+
+foreach(_runtime_dll IN LISTS RUNTIME_LIST)
     cmake_path(GET _runtime_dll PARENT_PATH _runtime_dll_path)
     cmake_path(NATIVE_PATH _runtime_dll_path _runtime_dll_path_native)
     list(APPEND _path "${_runtime_dll_path_native}")

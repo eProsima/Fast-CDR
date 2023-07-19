7313aadc6d7f4ae9989a2bb07d288f091115a1ff
Ricardo González Moreno <ricardo@richiware.dev>
2023-07-19 15:48:06 +0200

    Refs #18687. Add missing header licenses
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- cmake/testing/generate_google_test_win_wrapper.cmake
+++ cmake/testing/generate_google_test_win_wrapper.cmake
@@ -1,3 +1,17 @@
+# Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 if (NOT DEFINED TARGET)
     message(FATAL_ERROR "This scrips needs TARGET variable set")
 endif()
@@ -24,4 +38,3 @@ file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_win_wrapper_${CONFIG}.bat" "
 set \"PATH=${_path};%PATH%\"
 \"${_cmake_command}\" %*
 ")
-

8463a382108093e1c80c80098cacb0838d9d16e1
Ricardo González Moreno <ricardo@richiware.dev>
2023-07-20 11:36:26 +0200

    Refs #18687. Add regression test
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/xcdr/xcdrv1.cpp
+++ test/xcdr/xcdrv1.cpp
@@ -35,6 +35,66 @@ class XCdrv1PLTest : public ::testing::TestWithParam< Cdr::XCdrHeaderSelection>
 {
 };
 
+template<class _T>
+void xcdrv1_serialize_the_value(
+        const XCdrStreamValues& expected_streams,
+        Cdr::XCdrHeaderSelection header_selection,
+        _T value)
+{
+    //{ Prepare buffer
+    EncodingAlgorithmFlag encoding = EncodingAlgorithmFlag::PL_CDR;
+    Cdr::Endianness endianness = Cdr::Endianness::LITTLE_ENDIANNESS;
+    uint8_t tested_stream = 0 + static_cast<uint8_t>(header_selection);
+    size_t total_size = expected_streams[tested_stream].size() + 4; // Extra needed space for temp LongMemberHeader
+    auto buffer =
+            std::unique_ptr<char, void (*)(
+        void*)>{reinterpret_cast<char*>(calloc(total_size, sizeof(char))), free};
+    FastBuffer fast_buffer(buffer.get(), total_size);
+    Cdr cdr(fast_buffer, endianness, CdrVersion::XCDRv1);
+    //}
+
+    //{ Encode optional not present.
+    cdr.set_encoding_flag(encoding);
+    cdr.serialize_encapsulation();
+    Cdr::state enc_state(cdr);
+    cdr.begin_serialize_type(enc_state, encoding);
+    cdr.serialize_member(MemberId(1), value, header_selection);
+    cdr.end_serialize_type(enc_state);
+    Cdr::state enc_state_end(cdr);
+    //}
+
+    //{ Test encoded content
+    ASSERT_EQ(cdr.get_serialized_data_length(), expected_streams[tested_stream].size());
+    ASSERT_EQ(0, memcmp(buffer.get(), expected_streams[tested_stream].data(), expected_streams[tested_stream].size()));
+    //}
+
+    //{ Decoding optional not present
+    _T dec_value {0};
+    cdr.reset();
+    cdr.read_encapsulation();
+    ASSERT_EQ(cdr.get_encoding_flag(), encoding);
+    ASSERT_EQ(cdr.endianness(), endianness);
+    cdr.deserialize_type(encoding, [&dec_value](Cdr& cdr_inner, const MemberId& mid) -> bool
+            {
+                bool ret_value = true;
+                switch (mid.id)
+                {
+                    case 1:
+                        cdr_inner.deserialize_member(dec_value);
+                        break;
+                    default:
+                        ret_value = false;
+                        break;
+                }
+
+                return ret_value;
+            });
+    ASSERT_EQ(value, dec_value);
+    Cdr::state dec_state_end(cdr);
+    ASSERT_EQ(enc_state_end, dec_state_end);
+    //}
+}
+
 TEST_P(XCdrv1Test, auto_selection_on_decode)
 {
     EncodingAlgorithmFlag encoding = std::get<0>(GetParam());
@@ -128,55 +188,52 @@ TEST_P(XCdrv1PLTest, pl_octet_opt_member)
     };
     //}
 
-    //{ Prepare buffer
-    EncodingAlgorithmFlag encoding = EncodingAlgorithmFlag::PL_CDR;
-    Cdr::Endianness endianness = Cdr::Endianness::LITTLE_ENDIANNESS;
-    uint8_t tested_stream = 0 + static_cast<uint8_t>(GetParam());
-    size_t total_size = expected_streams[tested_stream].size() + 4; // Extra needed space for temp LongMemberHeader
-    auto buffer =
-            std::unique_ptr<char, void (*)(
-        void*)>{reinterpret_cast<char*>(calloc(total_size
-                , sizeof(char))), free};
-    FastBuffer fast_buffer(buffer.get(), total_size);
-    Cdr cdr(fast_buffer, endianness, CdrVersion::XCDRv1);
+    Cdr::XCdrHeaderSelection header_selection {GetParam()};
+    xcdrv1_serialize_the_value(expected_streams, header_selection, octet_value);
+}
+
+TEST_P(XCdrv1PLTest, pl_long_opt_member)
+{
+    constexpr int32_t long_value {static_cast<int32_t>(0xDCCDCDCD)};
+    constexpr uint8_t ival {0xCD};
+    constexpr uint8_t fval {0xDC};
+
+    //{ Defining expected XCDR streams
+    XCdrStreamValues expected_streams;
+    expected_streams[0 + Cdr::XCdrHeaderSelection::SHORT_HEADER] =
+    {
+        0x00, 0x03, 0x00, 0x00, // Encapsulation
+        0x01, 0x00, 0x04, 0x00, // ShortMemberHeader
+        ival, ival, ival, fval, // Long
+        0x02, 0x3F, 0x00, 0x00  // Sentinel
+    };
+    expected_streams[0 + Cdr::XCdrHeaderSelection::LONG_HEADER] =
+    {
+        0x00, 0x03, 0x00, 0x00, // Encapsulation
+        0x01, 0x3F, 0x08, 0x00, // LongMemberHeader
+        0x01, 0x00, 0x00, 0x00, // Member ID and flags
+        0x04, 0x00, 0x00, 0x00, // Size
+        ival, ival, ival, fval, // Long
+        0x02, 0x3F, 0x00, 0x00  // Sentinel
+    };
+    expected_streams[0 + Cdr::XCdrHeaderSelection::AUTO_WITH_SHORT_HEADER_BY_DEFAULT] =
+    {
+        0x00, 0x03, 0x00, 0x00, // Encapsulation
+        0x01, 0x00, 0x04, 0x00, // ShortMemberHeader
+        ival, ival, ival, fval, // Long
+        0x02, 0x3F, 0x00, 0x00  // Sentinel
+    };
+    expected_streams[0 + Cdr::XCdrHeaderSelection::AUTO_WITH_LONG_HEADER_BY_DEFAULT] =
+    {
+        0x00, 0x03, 0x00, 0x00, // Encapsulation
+        0x01, 0x00, 0x04, 0x00, // ShortMemberHeader
+        ival, ival, ival, fval, // Long
+        0x02, 0x3F, 0x00, 0x00  // Sentinel
+    };
     //}
 
-    //{ Encode optional not present.
-    optional<uint8_t> opt_value {octet_value};
-    cdr.set_encoding_flag(encoding);
-    cdr.serialize_encapsulation();
-    Cdr::state enc_state(cdr);
-    cdr.begin_serialize_type(enc_state, encoding);
-    cdr.serialize_member(MemberId(1), opt_value, GetParam());
-    cdr.end_serialize_type(enc_state);
-    Cdr::state enc_state_end(cdr);
-    //}
-
-    //{ Test encoded content
-    ASSERT_EQ(cdr.get_serialized_data_length(), expected_streams[tested_stream].size());
-    ASSERT_EQ(0, memcmp(buffer.get(), expected_streams[tested_stream].data(), expected_streams[tested_stream].size()));
-    //}
-
-    //{ Decoding optional not present
-    optional<uint8_t> opt_dec_value = 0;
-    cdr.reset();
-    cdr.read_encapsulation();
-    ASSERT_EQ(cdr.get_encoding_flag(), encoding);
-    ASSERT_EQ(cdr.endianness(), endianness);
-    cdr.deserialize_type(encoding, [&opt_dec_value](Cdr& cdr_inner, const MemberId& mid) -> bool
-            {
-                switch (mid.id)
-                {
-                    case 1:
-                        cdr_inner.deserialize_member(opt_dec_value);
-                }
-
-                return true;
-            });
-    ASSERT_EQ(opt_value, opt_dec_value);
-    Cdr::state dec_state_end(cdr);
-    ASSERT_EQ(enc_state_end, dec_state_end);
-    //}
+    Cdr::XCdrHeaderSelection header_selection {GetParam()};
+    xcdrv1_serialize_the_value(expected_streams, header_selection, long_value);
 }
 
 INSTANTIATE_TEST_SUITE_P(
--- test/xcdr/xcdrv2.cpp
+++ test/xcdr/xcdrv2.cpp
@@ -35,6 +35,69 @@ class XCdrv2PLTest : public ::testing::TestWithParam< Cdr::XCdrHeaderSelection>
 {
 };
 
+template<class _T>
+void xcdrv2_serialize_the_value(
+        const XCdrStreamValues& expected_streams,
+        Cdr::XCdrHeaderSelection header_selection,
+        _T value,
+        uint8_t last_octet)
+{
+    //{ Prepare buffer
+    EncodingAlgorithmFlag encoding = EncodingAlgorithmFlag::PL_CDR2;
+    Cdr::Endianness endianness = Cdr::Endianness::LITTLE_ENDIANNESS;
+    uint8_t tested_stream = 0 + static_cast<uint8_t>(header_selection);
+    size_t total_size = expected_streams[tested_stream].size() + 4; // Extra needed space for temp LongMemberHeader
+    auto buffer =
+            std::unique_ptr<char, void (*)(
+        void*)>{reinterpret_cast<char*>(calloc(total_size, sizeof(char))), free};
+    FastBuffer fast_buffer(buffer.get(), total_size);
+    Cdr cdr(fast_buffer, endianness, CdrVersion::XCDRv2);
+    //}
+
+    //{ Encode optional not present.
+    cdr.set_encoding_flag(encoding);
+    cdr.serialize_encapsulation();
+    Cdr::state enc_state(cdr);
+    cdr.begin_serialize_type(enc_state, encoding);
+    cdr.serialize_member(MemberId(1), value, header_selection);
+    cdr.end_serialize_type(enc_state);
+    Cdr::state enc_state_end(cdr);
+    //}
+
+    //{ Test encoded content
+    auto it = std::find(expected_streams[tested_stream].begin(), expected_streams[tested_stream].end(), last_octet);
+    ASSERT_EQ(cdr.get_serialized_data_length(),
+            static_cast<size_t>(std::distance(expected_streams[tested_stream].begin(), ++it)));
+    ASSERT_EQ(0, memcmp(buffer.get(), expected_streams[tested_stream].data(), expected_streams[tested_stream].size()));
+    //}
+
+    //{ Decoding optional not present
+    _T dec_value {0};
+    cdr.reset();
+    cdr.read_encapsulation();
+    ASSERT_EQ(cdr.get_encoding_flag(), encoding);
+    ASSERT_EQ(cdr.endianness(), endianness);
+    cdr.deserialize_type(encoding, [&dec_value](Cdr& cdr_inner, const MemberId& mid) -> bool
+            {
+                bool ret_value = true;
+                switch (mid.id)
+                {
+                    case 1:
+                        cdr_inner.deserialize_member(dec_value);
+                        break;
+                    default:
+                        ret_value = false;
+                        break;
+                }
+
+                return ret_value;
+            });
+    ASSERT_EQ(value, dec_value);
+    Cdr::state dec_state_end(cdr);
+    ASSERT_EQ(enc_state_end, dec_state_end);
+    //}
+}
+
 TEST_P(XCdrv2Test, auto_selection_on_decode)
 {
     EncodingAlgorithmFlag encoding = std::get<0>(GetParam());
@@ -123,56 +186,51 @@ TEST_P(XCdrv2PLTest, pl_octet_opt_member)
     };
     //}
 
-    //{ Prepare buffer
-    EncodingAlgorithmFlag encoding = EncodingAlgorithmFlag::PL_CDR2;
-    Cdr::Endianness endianness = Cdr::Endianness::LITTLE_ENDIANNESS;
-    uint8_t tested_stream = 0 + static_cast<uint8_t>(GetParam());
-    size_t total_size = expected_streams[tested_stream].size() + 4; // Extra needed space for temp LongMemberHeader
-    auto buffer =
-            std::unique_ptr<char, void (*)(
-        void*)>{reinterpret_cast<char*>(calloc(total_size, sizeof(char))), free};
-    FastBuffer fast_buffer(buffer.get(), total_size);
-    Cdr cdr(fast_buffer, endianness, CdrVersion::XCDRv2);
+    Cdr::XCdrHeaderSelection header_selection {GetParam()};
+    xcdrv2_serialize_the_value(expected_streams, header_selection, octet_value, octet_value);
+}
+
+TEST_P(XCdrv2PLTest, pl_long_opt_member)
+{
+    constexpr int32_t long_value {static_cast<int32_t>(0xDCCDCDCD)};
+    constexpr uint8_t ival {0xCD};
+    constexpr uint8_t fval {0xDC};
+
+    //{ Defining expected XCDR streams
+    XCdrStreamValues expected_streams;
+    expected_streams[0 + Cdr::XCdrHeaderSelection::SHORT_HEADER] =
+    {
+        0x00, 0x0b, 0x00, 0x00, // Encapsulation
+        0x08, 0x00, 0x00, 0x00, // DHEADER
+        0x01, 0x00, 0x00, 0x20, // EMHEADER1(M) without NEXTINT
+        ival, ival, ival, fval  // Long
+    };
+    expected_streams[0 + Cdr::XCdrHeaderSelection::LONG_HEADER] =
+    {
+        0x00, 0x0b, 0x00, 0x00, // Encapsulation
+        0x0c, 0x00, 0x00, 0x00, // DHEADER
+        0x01, 0x00, 0x00, 0x40, // EMHEADER1(M) with NEXTINT
+        0x04, 0x00, 0x00, 0x00, // Member size
+        ival, ival, ival, fval  // Long
+    };
+    expected_streams[0 + Cdr::XCdrHeaderSelection::AUTO_WITH_SHORT_HEADER_BY_DEFAULT] =
+    {
+        0x00, 0x0b, 0x00, 0x00, // Encapsulation
+        0x08, 0x00, 0x00, 0x00, // DHEADER
+        0x01, 0x00, 0x00, 0x20, // EMHEADER1(M) without NEXTINT
+        ival, ival, ival, fval  // Long
+    };
+    expected_streams[0 + Cdr::XCdrHeaderSelection::AUTO_WITH_LONG_HEADER_BY_DEFAULT] =
+    {
+        0x00, 0x0b, 0x00, 0x00, // Encapsulation
+        0x08, 0x00, 0x00, 0x00, // DHEADER
+        0x01, 0x00, 0x00, 0x20, // EMHEADER1(M) without NEXTINT
+        ival, ival, ival, fval  // Long
+    };
     //}
 
-    //{ Encode optional not present.
-    optional<uint8_t> opt_value {octet_value};
-    cdr.set_encoding_flag(encoding);
-    cdr.serialize_encapsulation();
-    Cdr::state enc_state(cdr);
-    cdr.begin_serialize_type(enc_state, encoding);
-    cdr.serialize_member(MemberId(1), opt_value, GetParam());
-    cdr.end_serialize_type(enc_state);
-    Cdr::state enc_state_end(cdr);
-    //}
-
-    //{ Test encoded content
-    auto it = std::find(expected_streams[tested_stream].begin(), expected_streams[tested_stream].end(), octet_value);
-    ASSERT_EQ(cdr.get_serialized_data_length(),
-            static_cast<size_t>(std::distance(expected_streams[tested_stream].begin(), ++it)));
-    ASSERT_EQ(0, memcmp(buffer.get(), expected_streams[tested_stream].data(), expected_streams[tested_stream].size()));
-    //}
-
-    //{ Decoding optional not present
-    optional<uint8_t> opt_dec_value = 0;
-    cdr.reset();
-    cdr.read_encapsulation();
-    ASSERT_EQ(cdr.get_encoding_flag(), encoding);
-    ASSERT_EQ(cdr.endianness(), endianness);
-    cdr.deserialize_type(encoding, [&opt_dec_value](Cdr& cdr_inner, const MemberId& mid) -> bool
-            {
-                switch (mid.id)
-                {
-                    case 1:
-                        cdr_inner.deserialize_member(opt_dec_value);
-                }
-
-                return true;
-            });
-    ASSERT_EQ(opt_value, opt_dec_value);
-    Cdr::state dec_state_end(cdr);
-    ASSERT_EQ(enc_state_end, dec_state_end);
-    //}
+    Cdr::XCdrHeaderSelection header_selection {GetParam()};
+    xcdrv2_serialize_the_value(expected_streams, header_selection, long_value, fval);
 }
 
 INSTANTIATE_TEST_SUITE_P(

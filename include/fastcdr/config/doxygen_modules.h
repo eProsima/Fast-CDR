fe726a8490f1e24b94988cd7252ac98b704142c7
Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
2024-02-21 13:46:56 +0100

    Add doxygen_modules.h header file (#194)
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

---
+++ include/fastcdr/config/doxygen_modules.h
@@ -0,0 +1,34 @@
+// Copyright 2024 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file doxygen_modules.h
+ */
+
+#ifndef FASTCDR_DOXYGEN_MODULES_H_
+#define FASTCDR_DOXYGEN_MODULES_H_
+
+/*!
+ * @defgroup FASTCDRAPIREFERENCE FastCDR Serializer API Reference
+ * @brief FastCDR Serializer internal API groped in modules.
+ */
+
+/*!
+ * @defgroup EXCEPTIONMODULE Exceptions
+ * @ingroup FASTCDRAPIREFERENCE
+ * @brief Exceptions used by the FastCDR API.
+ * All exceptions defined in this module are thrown by the FastCDR library.
+ */
+
+#endif /* FASTCDR_DOXYGEN_MODULES_H_ */

5f5b497ae7821d48c4158b1a4cf10ddd5f788109
Ricardo González Moreno <ricardo@richiware.dev>
2026-07-21 12:35:33 +0200

    Fix
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/cdr/array_as_std_vector.cpp
+++ test/cdr/array_as_std_vector.cpp
@@ -267,6 +267,75 @@ TEST_P(CdrArrayAsSTDVectorTest, array_ulong_as_std_vector)
 }
 
 /*!
+ * @test Test an array of boolean type stored in a std::vector.
+ */
+TEST_P(CdrArrayAsSTDVectorTest, array_bool_as_std_vector)
+{
+    const std::vector<bool> array_value {true, false};
+    constexpr uint8_t tval {0x01};
+    constexpr uint8_t fval {0x00};
+
+    //{ Defining expected XCDR streams
+    XCdrStreamValues expected_streams;
+    expected_streams[0 + EncodingAlgorithmFlag::PLAIN_CDR + Cdr::Endianness::BIG_ENDIANNESS] =
+    {
+        0x00, 0x00, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PLAIN_CDR + Cdr::Endianness::LITTLE_ENDIANNESS] =
+    {
+        0x00, 0x01, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PL_CDR + Cdr::Endianness::BIG_ENDIANNESS] =
+    {
+        0x00, 0x02, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PL_CDR + Cdr::Endianness::LITTLE_ENDIANNESS] =
+    {
+        0x00, 0x03, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PLAIN_CDR2 + Cdr::Endianness::BIG_ENDIANNESS] =
+    {
+        0x00, 0x06, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PLAIN_CDR2 + Cdr::Endianness::LITTLE_ENDIANNESS] =
+    {
+        0x00, 0x07, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::DELIMIT_CDR2 + Cdr::Endianness::BIG_ENDIANNESS] =
+    {
+        0x00, 0x08, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::DELIMIT_CDR2 + Cdr::Endianness::LITTLE_ENDIANNESS] =
+    {
+        0x00, 0x09, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PL_CDR2 + Cdr::Endianness::BIG_ENDIANNESS] =
+    {
+        0x00, 0x0A, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    expected_streams[0 + EncodingAlgorithmFlag::PL_CDR2 + Cdr::Endianness::LITTLE_ENDIANNESS] =
+    {
+        0x00, 0x0B, 0x00, 0x00, // Encapsulation
+        tval, fval              // Boolean
+    };
+    //}
+
+    EncodingAlgorithmFlag encoding = std::get<0>(GetParam());
+    Cdr::Endianness endianness = std::get<1>(GetParam());
+
+    serialize_array(expected_streams, encoding, endianness, array_value);
+}
+
+/*!
  * @test Test an array of struct type stored in a std::vector.
  * @code{.idl}
  * struct InnerBasicTypesShortStruct
